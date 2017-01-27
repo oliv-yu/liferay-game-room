89580f0c3bca13bce64f42bde95e0b4af38b9cd8
antoniopol06 <antonio.pol@liferay.com>
2017-01-27 13:21:41 -0800

    stream image

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
+var ss = require('socket.io-stream');
+
+var path = require('path');
+
+var fs = require('fs');
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/camera-example/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
 	socket.on('test', function(data){
 		console.log(data);
+	});
+
+	ss(socket).on('image', function(stream, data) {
+		socket.broadcast.emit('update-room', data);
 	});
 });
 
