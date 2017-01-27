f4f83b30c4a79253e07c04da640a97beeb698526
antoniopol06 <antonio.pol@liferay.com>
2017-01-27 12:27:49 -0800

    socket.io event example

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var express = require('express');
+var express = require('express')
+
+var http = require('http');
+
 var app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/camera-example/index.html');
@@ -10,10 +14,14 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+	socket.on('test', function(data){
+		console.log(data);
+	});
 });
+
 
 app.use(express.static('public'));
 
-app.listen(9000, function () {
+server.listen(9000, function () {
   console.log('Liferay Game Room on 9000');
 });
