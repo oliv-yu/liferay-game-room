3fcbbf960f2c765580280abd95ce2ec1dc4e78c6
Patrick Yeo <patrick.yeo@liferay.com>
2017-01-27 11:56:10 -0800

    Add socket.io

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	res.sendFile(__dirname + '/camera-example/index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+app.use(express.static('public'));
 
 app.listen(9000, function () {
   console.log('Liferay Game Room on 9000');
